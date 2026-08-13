aada43b2ef3cd9e60e0412623b8863400aae5390
elman <elman.muradov17@gmail.com>
2026-08-14 03:20:01 +0400

    0-async

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -7,7 +7,7 @@ from typing import AsyncGenerator
 
 
 async def async_generator() -> AsyncGenerator[float, None]:
-    """Loops 100 times"""
+    """Loops 10 times, waits 1 second"""
     for _ in range(10):
         await asyncio.sleep(1)
         yield random.uniform(0, 10)
