c58f4c9791de023c761395cf742a4bb3f43eae35
elman <elman.muradov17@gmail.com>
2026-09-05 12:15:09 +0400

    1-simple_pagination

---
+++ pagination/1-simple_pagination.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+
+"""Simple pagination"""
+
+import csv
+from typing import List, Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Calculates the start and end indexes for a given page and page_size"""
+    start_index = (page - 1) * page_size
+    end_index = page * page_size
+    return (start_index, end_index)
+
+
+class Server:
+    """Server class to paginate a database"""
+
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """Cached dataset"""
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                self.__dataset = [row for row in reader]
+
+        return self.__dataset
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Return a page of the dataset"""
+        assert isinstance(page, int) and page > 0
+        assert isinstance(page_size, int) and page_size > 0
+
+        start_index, end_index = index_range(page, page_size)
+        data = self.dataset()
+
+        if start_index >= len(data):
+            return []
+
+        return data[start_index:end_index]
