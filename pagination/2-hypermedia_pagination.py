80f46fea575603731352ef85f2cab054f1ef2478
elman <elman.muradov17@gmail.com>
2026-09-07 19:24:18 +0400

    2-hypermedia_pagination

--- pagination/2-hypermedia_pagination.py
+++ pagination/2-hypermedia_pagination.py
@@ -25,11 +25,11 @@ class Server:
     def dataset(self) -> List[List]:
         """Cache dataset"""
 
-        if self.__dataset__ is None:
+        if self.__dataset is None:
             with open(self.DATA_FILE) as f:
                 reader = csv.reader(f)
                 dataset = [row for row in reader]
-            self.__dataset__ = self.dataset[1:]
+            self.__dataset = self.dataset[1:]
 
         return self.__dataset
 
